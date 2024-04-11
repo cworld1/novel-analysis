590c9afbf759bb6df6cefc979948fea684d08326
CWorld <cworld0@qq.com>
2024-04-11 19:15:26 +0800

    feat(components): :sparkles: add follow system support; add more settings; update settings ui

--- web/src/components/ThemeProvider.tsx
+++ web/src/components/ThemeProvider.tsx
@@ -1,27 +1,35 @@
 import React, { useState } from "react";
 import { ConfigProvider, theme } from "antd";
-import { MapToken } from "antd/es/theme/interface";
 
 export const ThemeContext = React.createContext({
   currentTheme: "light",
-  setCurrentTheme: (_theme: "light" | "dark") => {},
+  setCurrentTheme: (_theme: string) => {},
+  colorPrimary: "#1677ff",
+  setColorPrimary: (_color: string) => {},
 });
 
-export interface ThemeState {
-  currentTheme: MapToken;
-}
-
 export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
-  const [currentTheme, setCurrentTheme] = useState<"light" | "dark">("light");
+  const [currentTheme, setCurrentTheme] = useState<string>("light");
+  const [colorPrimary, setColorPrimary] = useState<string>("#1677ff");
 
   return (
-    <ThemeContext.Provider value={{ currentTheme, setCurrentTheme }}>
+    <ThemeContext.Provider
+      value={{ currentTheme, setCurrentTheme, colorPrimary, setColorPrimary }}
+    >
       <ConfigProvider
         theme={{
           algorithm:
-            currentTheme === "light"
-              ? theme.defaultAlgorithm
-              : theme.darkAlgorithm,
+            currentTheme === "follow_system"
+              ? window.matchMedia &&
+                window.matchMedia("(prefers-color-scheme: dark)").matches
+                ? theme.darkAlgorithm
+                : theme.defaultAlgorithm
+              : currentTheme === "dark"
+              ? theme.darkAlgorithm
+              : theme.defaultAlgorithm,
+          token: {
+            colorPrimary: colorPrimary,
+          },
         }}
       >
         {children}
--- web/src/pages/CommonSettings.tsx
+++ web/src/pages/CommonSettings.tsx
@@ -1,33 +1,97 @@
 import React, { useContext } from "react";
-
-import {
-  Button,
-  Space,
-  Input,
-  ColorPicker,
-  Divider,
-  Switch,
-  theme,
-} from "antd";
+// Antd
+import { Button, ColorPicker, List, Select, Typography } from "antd";
+const { Title } = Typography;
+// Components
 import { ThemeContext } from "../components/ThemeProvider";
+import {
+  BgColorsOutlined,
+  DeleteOutlined,
+  ReloadOutlined,
+  SkinOutlined,
+} from "@ant-design/icons";
 
 const SettingsPage: React.FC = () => {
-  const { currentTheme, setCurrentTheme } = useContext(ThemeContext);
+  const { currentTheme, setCurrentTheme, colorPrimary, setColorPrimary } =
+    useContext(ThemeContext);
+
+  const appearanceData = [
+    {
+      title: "Primary color",
+      avatar: <BgColorsOutlined style={{ fontSize: 27, height: 48 }} />,
+      desc: "Change the primary color of the application",
+      actions: [
+        <ColorPicker
+          showText
+          value={colorPrimary}
+          onChangeComplete={(color) => setColorPrimary(color.toHex())}
+        />,
+      ],
+    },
+    {
+      title: "Color scheme",
+      avatar: <SkinOutlined style={{ fontSize: 27, height: 48 }} />,
+      desc: "Change the color scheme of the application",
+      actions: [
+        <Select
+          style={{ width: 140 }}
+          defaultValue={currentTheme}
+          onChange={setCurrentTheme}
+          options={[
+            { value: "light", label: "Light" },
+            { value: "dark", label: "Dark" },
+            { value: "follow_system", label: "Follow system" },
+          ]}
+        />,
+      ],
+    },
+  ];
+
+  const databaseData = [
+    {
+      title: "Update database",
+      avatar: <ReloadOutlined style={{ fontSize: 27, height: 48 }} />,
+      desc: "Update the database schema to the latest version",
+      actions: [
+        <Button>Update force</Button>,
+        <Button type="primary">Update</Button>,
+      ],
+    },
+    {
+      title: "Clear darabase",
+      avatar: <DeleteOutlined style={{ fontSize: 27, height: 48 }} />,
+      desc: "Clear the database and reset to the default state",
+      actions: [<Button danger>Clear</Button>],
+    },
+  ];
+
+  const renderListItem = (item: any) => (
+    <List.Item actions={item.actions}>
+      <List.Item.Meta
+        avatar={item.avatar}
+        title={item.title}
+        description={item.desc}
+      />
+    </List.Item>
+  );
+
   return (
     <>
-      {/* <ColorPicker showText value={color} onChangeComplete={} /> */}
-      <Divider />
-      <Space>
-        <Switch
-          checked={currentTheme === "dark"}
-          onChange={(checked) => setCurrentTheme(checked ? "dark" : "light")}
-        />
-      </Space>
-      <Divider />
-      <Space>
-        <Input placeholder="Please Input" />
-        <Button type="primary">Submit</Button>
-      </Space>
+      <Title>Settings</Title>
+      <Title level={4}>Appearance</Title>
+      <List
+        bordered
+        itemLayout="horizontal"
+        dataSource={appearanceData}
+        renderItem={renderListItem}
+      />
+      <Title level={4}>Database</Title>
+      <List
+        bordered
+        itemLayout="horizontal"
+        dataSource={databaseData}
+        renderItem={renderListItem}
+      />
     </>
   );
 };
