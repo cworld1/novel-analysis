85ffb73991cc7e653d3fef789697a734a57f250b
CWorld <cworld0@qq.com>
2024-04-11 17:25:02 +0800

    feat(pages): :sparkles: add spin loading effect

--- web/src/pages/BoardType.tsx
+++ web/src/pages/BoardType.tsx
@@ -1,40 +1,63 @@
 import React, { useEffect } from "react";
 // Antd
-import { Typography } from "antd";
-const { Title } = Typography;
+import { Spin, theme, Typography } from "antd";
+const { Title, Paragraph } = Typography;
 // Echarts
 import axios from "axios";
 import * as echarts from "echarts";
 
-const getImage = async (shape: string) => {
-  var chart = echarts.init(document.getElementById(shape), "white", {
-    renderer: "canvas",
-  });
-  axios
-    .get(`http://127.0.0.1:5000/anal/type?shape=${shape}`)
-    .then(function (result) {
-      chart.setOption(result.data);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-};
-
 const BoardTypePage: React.FC = () => {
+  const {
+    token: { colorFillQuaternary, borderRadiusLG },
+  } = theme.useToken();
+
+  const getImage = async (shape: string) => {
+    axios
+      .get(`http://127.0.0.1:5000/anal/type?shape=${shape}`)
+      .then(function (result) {
+        var chart = echarts.init(document.getElementById(shape), "white", {
+          renderer: "canvas",
+        });
+        chart.setOption(result.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+
   useEffect(() => {
     const fetchData = async () => {
-      await getImage("bar");
-      await getImage("pie");
+      getImage("bar");
+      getImage("pie");
     };
 
     fetchData();
   }, []);
 
+  const chartStyle = {
+    maxWidth: 1000,
+    height: 500,
+    padding: 25,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: colorFillQuaternary,
+    borderRadius: borderRadiusLG,
+  };
+
   return (
     <>
       <Title>Type of novel</Title>
-      <div id="bar" style={{ width: 1000, height: 600 }} />
-      <div id="pie" style={{ width: 1000, height: 600 }} />
+      <Paragraph>
+        <div id="bar" style={chartStyle}>
+          <Spin />
+        </div>
+      </Paragraph>
+      <Paragraph>
+        <div id="pie" style={chartStyle}>
+          <Spin />
+        </div>
+      </Paragraph>
     </>
   );
 };
