c6713fe3c50f17ba785ab810ea5a30f567b9c2bd
Boiyaaa <836116040@qq.com>
2024-04-12 16:55:52 +0800

    添加utf-8

--- fetch/fetch_helper.py
+++ fetch/fetch_helper.py
@@ -2,6 +2,7 @@ import json
 
 
 def get_content(file_path):
-    with open(file_path, "r") as json_file:
+    with open(file_path, "r", encoding="utf-8") as json_file:
         file_content = json_file.read()
     return json.loads(file_content)
+
