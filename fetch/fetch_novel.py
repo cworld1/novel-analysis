f07af0b50ab7c7e5d952a3f79fa44fe7c1dd696f
Boiyaaa <836116040@qq.com>
2024-04-17 14:43:59 +0800

    fetch_novel

--- fetch/fetch_novel.py
+++ fetch/fetch_novel.py
@@ -1,6 +1,5 @@
 import os
 import random
-
 from fetch.fetch_helper import get_content
 
 
@@ -10,39 +9,45 @@ class FetchNovel:
         self.detailed_path = f"{path}/{sub_folder}"
 
     def fetch(self, choose: str, count: int = 1):
-        # Randomly choose one book
-        if choose == "random":
-            total_books = [
-                f
-                for f in os.listdir(self.detailed_path)
-                if os.path.isfile(os.path.join(self.detailed_path, f))
-            ]
-            if not total_books:
-                print("No books found!")
-                return None
-            # Randomly choose count number of books
-            books = random.sample(total_books, count)
-        # Choose the book by the id
-        elif choose is not None:
-            books = [f"{choose}.json"]
-        else:
-            print("No book id provided!")
-            return []
+        while True:
+            # Randomly choose one book
+            if choose == "random":
+                total_books = [
+                    f
+                    for f in os.listdir(self.detailed_path)
+                    if os.path.isfile(os.path.join(self.detailed_path, f))
+                ]
+                if not total_books:
+                    print("No books found!")
+                    return None
+                # Randomly choose count number of books
+                books = random.sample(total_books, count)
+            # Choose the book by the id
+            elif choose is not None:
+                books = [f"{choose}.json"]
+            else:
+                print("No book id provided!")
+                return []
 
-        books_infos = []
-        for book in books:
-            full_content = get_content(os.path.join(self.detailed_path, book))[
-                "bookInfo"
+            books_infos = []
+            output_comment_anal_filenames = [
+                filename.split(".")[0]  # 去除 .txt 后缀
+                for filename in os.listdir("./data/output_comment_anal")
             ]
-            selected_content = {
-                "bookId": full_content["bookId"],
-                "bookName": full_content["bookName"],
-                "authorName": full_content["authorName"],
-            }
-            books_infos.append(selected_content)
 
-        # Only get each book's necessary info
-        return books_infos
+            for book in books:
+                full_content = get_content(os.path.join(self.detailed_path, book))[
+                    "bookInfo"
+                ]
+                selected_content = {
+                    "bookId": full_content["bookId"],
+                    "bookName": full_content["bookName"],
+                    "authorName": full_content["authorName"],
+                }
+                # 如果书籍 ID 在 output_comment_anal 文件名中，则添加到 books_infos 中
+                if selected_content['bookId'] in output_comment_anal_filenames:
+                    books_infos.append(selected_content)
+                    return books_infos  # 如果符合条件，则直接返回
 
 
 # Test
