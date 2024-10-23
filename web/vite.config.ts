6b1e056d9b8f05dba97b192f8c2779c021609661
CWorld <cworld0@qq.com>
2024-10-23 23:34:17 +0800

    Update vite.config.ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/novel-analysis/'
+  base: '/novel-analysis/',
   plugins: [react()],
 })
